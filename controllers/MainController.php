740678d2368879ac73d8c1a045042d8d8cc765a8
Razzwan <razvanlomov@gmail.com>
2015-08-17 12:24:26 +0300

    Update MainController.php

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -8,24 +8,14 @@ class MainController extends Controller
 {
     public function indexAction()
     {
-        $this->render('index', [
-            'test' => 'Это test variable'
-        ]);
+        $this->render('index');
     }
 
     public function testAction()
     {
-        $con = new ConnectPDO();
-
-        /*if(mail("razvanlomov@gmail.com", "Test", "Test message")){
-            $this->render('error', [
-                'error' => 'Письмо отправлено'
-            ]);
-        } else {
-            $this->render('error', [
-                'error' => 'ХРен там'
-            ]);
-        }*/
+        $this->render('error', [
+            'error' => 'Это test variable'
+        ]);
 
     }
 }
