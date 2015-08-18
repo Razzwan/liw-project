34ae1f55a536329e11ef5f81c0998b8c8ea118e7
Razzwan <razvanlomov@gmail.com>
2015-08-18 05:43:45 +0300

    css

--- views/layouts/layout.php
+++ views/layouts/layout.php
@@ -12,22 +12,41 @@ use liw\core\Liw;
     <meta charset="UTF-8"/>
     <link rel="SHORTCUT ICON" href="/img/ico/favicon.ico" type="image/x-icon"/>
     <link rel="stylesheet" type="text/css" media="screen"
-          href="/css/bootstrap.min.css" />
+          href="/css/jquery-ui.min.css" />
+    <link rel="stylesheet" type="text/css" media="screen"
+          href="/css/jquery-ui.structure.min.css" />
     <link rel="stylesheet" type="text/css" media="screen"
           href="/css/bootstrap-theme.min.css" />
+    <link rel="stylesheet" type="text/css" media="screen"
+          href="/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" media="screen"
+          href="/css/site.css" />
     <link rel="stylesheet" type="text/css" media="screen"
           href="/css/dev.css" />
     <title><?=$this->title?></title>
 </head>
 <body>
 
-<div class = "left_menu_wrap"></div>
-<nav id= "left_menu">
-    <ul>
-        <li><a href = "/" id="logo">main</a></li>
-        <li><a href="/test" class="button">test</a></li>
-    </ul>    
-</nav>
+<div id="top-menu">
+    <h1><?=$this->title?></h1>
+
+    <div class="btn-group" role="group" aria-label="...">
+        <a href = "/" class="btn btn-default ">
+            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+            main
+        </a>
+        <a href="/test" class="btn btn-default">
+            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+            test
+        </a>
+        <a href="/reg" class="btn btn-default">
+            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+            регистрация
+        </a>
+    </div>
+</div>
+
+
 
 <div id="main_wrapper">
 
@@ -43,8 +62,9 @@ use liw\core\Liw;
 
 <div class="js-block">
     <script src="/js/jquery.js" type="text/javascript" ></script>
+    <script src="/js/jquery-ui.min.js" type="text/javascript" ></script>
     <script src="/js/bootstrap.min.js" type="text/javascript" ></script>
-    <script src="/js/js.js" type="text/javascript" ></script>
+    <script src="/js/dev.js" type="text/javascript" ></script>
 </div>
 
 <div id="tooltip"></div>
