621a07e101b00d6c31e0a3587693c4d59b353a0d
Razzwan <razvanlomov@gmail.com>
2015-08-18 04:39:35 +0300

    fix index

--- web/index.php
+++ web/index.php
@@ -5,15 +5,15 @@
  */
 define('DEVELOP', true);
 
-define("LIW_WEB", dirname(__DIR__) . DIRECTORY_SEPARATOR); //определяем папку проекта
+define("LIW_WEB", dirname(__DIR__)); //определяем папку проекта
 
-require __DIR__ . '/../vendor/autoload.php'; //Файл загрузчик модулей приложения
+require LIW_WEB . '/vendor/autoload.php'; //Файл загрузчик модулей приложения
 
 /**
  * Запуск приложения
  *
  * @var $app object liw\core\base\Router
  */
-(new \liw\core\App())->start(require LIW_WEB . 'config/config.php');
+(new \liw\core\App())->start(require LIW_WEB . '/config/config.php');
 
 
