c054ff7019e33d43f66779c76ff8a4d7be9d60c2
Razzwan <razvanlomov@gmail.com>
2015-09-12 15:21:33 +0300

    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,40 +3,29 @@ var browserSync = require('browser-sync').create(),
     autoprefixer = require('gulp-autoprefixer'),
     concatCss = require('gulp-concat-css'),
     minifyCss = require('gulp-minify-css'),
-    compass = require('gulp-compass');
+    conf = require('./gulp_conf.json')
+    ;
 
 gulp.task('connect', function() {
     browserSync.init({
-        proxy: 'blog.loc/'
+        proxy: conf.host
     });
-
-    gulp.watch('web/sass/**/*.scss', ['sass']);
 
     gulp.watch('web/assets/*.css', ['my_styles']);
 
-    gulp.watch('web/assets/**/*.css', ['bootstrap']);
+    gulp.watch('web/assets/bootstrap/*.css', ['bootstrap']);
 
-    gulp.watch('web/assets/**/*.css', ['semantic']);
+    gulp.watch('web/assets/semantic/*.css', ['semantic']);
 
     gulp.watch('blog/views/**/*.php').on('change', browserSync.reload);
 
     gulp.watch('web/js/**/*.js').on('change', browserSync.reload);
 });
 
-gulp.task('sass', function(){
-    gulp.src('web/sass/**/*.scss')
-        .pipe(compass({
-            config_file : 'config.rb',
-            css : 'web/assets',
-            sass : 'web/sass'
-        }))
-        .pipe(gulp.dest('web/assets'));
-});
-
 // my_styles
-gulp.task('bootstrap', function(){
-    gulp.src('web/assets/bootstrap/*.css')
-        .pipe(concatCss("css/with_bootstrap.css"))
+gulp.task('my_styles', function(){
+    gulp.src('web/assets/*.css')
+        .pipe(concatCss("css/style.css"))
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
