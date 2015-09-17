08ba66ec5264ca312eed02100d6dd34abcd055be
Razzwan <razvanlomov@gmail.com>
2015-09-17 09:46:49 +0300

    Добавил переменную разрешенных языков в конфиг. Добавил еще один файл с адресами маршрутов для упрощенного роутинга. Изменил название файла маршрутов "login"->"user"

---
+++ app/config/ways/all.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    ''              => ['Main::index'],       //Run MainController::indexAction();
+    '/'             => ['Main::index'],
+    '/error'        => ['Main::error'],
+    '/more'         => ['Main::more'],
+    '/install'      => ['Main::install'],
+    '/test'         => ['Main::test'],
+
+    '/registration' => ['User::registration'],
+    '/login'        => ['User::login'],
+
+    '/articles'     => ['Article::show_all'],
+];
