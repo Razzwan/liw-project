d0b7c8c9413957473d7f6b2470fbe1af3a40c64a
Razzwan <razvanlomov@gmail.com>
2015-09-19 21:52:39 +0300

    Обновил маршруты

--- app/config/ways/user.php
+++ app/config/ways/user.php
@@ -1,11 +1,11 @@
 <?php
 return $ways = [
-    ''              => ['Main:index'],
-    '/'             => ['Main:index'],
-    '/error'        => ['Main:error'],
-    '/more'         => ['Main:more'],
-    '/install'      => ['Main:install'],
+    ''              => ['Main::index'],
+    '/'             => ['Main::index'],
+    '/error'        => ['Main::error'],
+    '/more'         => ['Main::more'],
+    '/install'      => ['Main::install'],
 
-    '/logout'       => ['User:logout'],
-    '/user'         => ['User:index'], '/user/=id'
+    '/logout'       => ['User::logout'],
+    '/user'         => ['User::index'], '/user/=id'
 ];
